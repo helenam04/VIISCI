3ae7e98abcba663c0b8ea837961601c8240b8067
helenam04 <143406415+helenam04@users.noreply.github.com>
2023-10-07 15:56:20 +0000

    aa

--- assets/java/myscript.js
+++ assets/java/myscript.js
@@ -3,7 +3,7 @@ function Enviar() {
     var nome = document.getElementById("nomeid");
 
     if (nome.value != "") {
-        alert('Obrigado sr(a) ' + nome.value + ' os seus dados foram encaminhados com sucesso');
+        alert('Vai toma no cu senhor' + nome.value + ' os seus dados foram encaminhados com sucesso para um contrato com o capiroto');
     }
 
 }
