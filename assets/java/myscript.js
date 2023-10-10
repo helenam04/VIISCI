ea0f2682e57497f08df469882eec2d25b6a91803
helenam04 <143406415+helenam04@users.noreply.github.com>
2023-10-10 12:52:22 +0000

    Co-authored-by: Gabreurr <Gabreurr@users.noreply.github.com> aaa

--- assets/java/myscript.js
+++ assets/java/myscript.js
@@ -3,7 +3,7 @@ function Enviar() {
     var nome = document.getElementById("nomeid");
 
     if (nome.value != "") {
-        alert('Vai toma no cu senhor' + nome.value + ' os seus dados foram encaminhados com sucesso para um contrato com o capiroto');
+        alert('oi ' + nome.value + ' oi');
     }
 
 }
